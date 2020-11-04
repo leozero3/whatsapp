9bc1e721cea00f3ce15eb78b320d1369598021eb
leozero3 <realleo2009@hotmail.com>
2020-11-04 17:57:46 -0300

    cadastro

---
+++ lib/Cadastro.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'model/Usuario.dart';
+
+class Cadastro extends StatefulWidget {
+  @override
+  _CadastroState createState() => _CadastroState();
+}
+
+class _CadastroState extends State<Cadastro> {
+  /// CONTROLADORES
+
+  TextEditingController _controllerNome = TextEditingController();
+  TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController _controllerSenha = TextEditingController();
+
+  String _mensagemErro = '';
+
+  ///valida  nome/email/senha
+  _validarCampos() {
+    //recupera dados dos campos
+    String nome = _controllerNome.text;
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    if (nome.length >= 3) {
+      if (email.isNotEmpty && email.contains('@')) {
+        if (senha.length >= 6) {
+          setState(() {
+            _mensagemErro = '';
+          });
+
+          Usuario usuario = Usuario();
+          usuario.nome = nome;
+          usuario.email = email;
+          usuario.senha = senha;
+
+          _cadastrarUsuario(usuario);
+        } else {
+          setState(() {
+            _mensagemErro = 'Preencha a senha';
+          });
+        }
+      } else {
+        setState(() {
+          _mensagemErro = 'Preencha o Email';
+        });
+      }
+    } else {
+      setState(() {
+        _mensagemErro = 'Preencha o Nome';
+      });
+    }
+  }
+
+  _cadastrarUsuario(Usuario usuario) {
+    FirebaseAuth auth = FirebaseAuth.instance; // simplifica o codigo pra 'auth'
+
+    auth.createUserWithEmailAndPassword(
+        email: usuario.email, password: usuario.senha).then((firebaseUser) {
+      setState(() {
+        _mensagemErro = 'Sucesso ao cadastrar';
+      });
+    }).catchError((erro){
+      setState(() {
+        _mensagemErro = 'erro ao cadastrar usuario';
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('cadastro'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: Color(0xff075e54)),
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 32),
+                  child: Image.asset(
+                    'assets/imagens/usuario.png',
+                    width: 200,
+                    height: 100,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: TextFormField(
+                    controller: _controllerNome,
+                    autofocus: true,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: 'Nome',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: TextFormField(
+                    controller: _controllerEmail,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: 'E-mail',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
+                  ),
+                ),
+                TextFormField(
+                  controller: _controllerSenha,
+                  obscureText: true,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: 'Senha',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(32))),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 10),
+                  child: RaisedButton(
+                    child: Text(
+                      'Cadastrar',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    color: Colors.green,
+                    padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32)),
+                    onPressed: () {
+                      _validarCampos();
+                    },
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    _mensagemErro,
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'Home.dart';
+import 'package:whatsapp/Login.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  FirebaseFirestore.instance.collection('collectionPath').doc('3').set({'Nome': 'isaac'});
+  //FirebaseFirestore.instance.collection('collectionPath').doc('3').set({'Nome': 'isaac'});
 
   runApp(MaterialApp(
-    home: Home(),
+    home: Login(),
+    theme: ThemeData(
+      primaryColor: Color(0xff075E54),
+      accentColor: Color(0xff25D366)
+    ),
     debugShowCheckedModeBanner: false,
   ));
 }
