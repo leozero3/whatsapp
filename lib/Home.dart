324c63c28847479c87cd96b1d3783e9e593b8a16
leozero3 <realleo2009@hotmail.com>
2020-11-09 21:08:35 -0300

    deslogar

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsapp/Login.dart';
 import 'package:whatsapp/telas/AbaContatos.dart';
 import 'package:whatsapp/telas/AbaConversas.dart';
 
@@ -10,6 +11,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   TabController _tabController;
+
+  List<String> itensMenu = ['Configurações', 'Deslogar'];
 
   String _emailUser = '';
 
@@ -28,32 +31,67 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     _tabController = TabController(length: 2, vsync: this);
   }
 
+  _escolhaMenuItem(String itemEscolhido) {
+    switch (itemEscolhido) {
+      case 'Configurações':
+        print('Configurações');
+        break;
+      case 'Deslogar':
+        _deslogarUsuario();
+        break;
+    }
+    //print('item: ' + itemEscolhido);
+  }
+
+  _deslogarUsuario() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    await auth.signOut();
+
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('WhatsApp'),
-          bottom: TabBar(
-            indicatorWeight: 4,
-            indicatorColor: Colors.white,
-            labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            controller: _tabController,
-            tabs: [
-              Tab(
-                text: 'Conversas',
-              ),
-              Tab(
-                text: 'Contatos',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
+      appBar: AppBar(
+        title: Text('WhatsApp'),
+        bottom: TabBar(
+          indicatorWeight: 4,
+          indicatorColor: Colors.white,
+          labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           controller: _tabController,
-          children: [
-            AbaConversas(),
-            AbaContatos(),
+          tabs: [
+            Tab(
+              text: 'Conversas',
+            ),
+            Tab(
+              text: 'Contatos',
+            ),
           ],
-        ));
+        ),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: _escolhaMenuItem,
+            itemBuilder: (context) {
+              return itensMenu.map(
+                (String item) {
+                  return PopupMenuItem<String>(
+                    value: item,
+                    child: Text(item),
+                  );
+                },
+              ).toList();
+            },
+          ),
+        ],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          AbaConversas(),
+          AbaContatos(),
+        ],
+      ),
+    );
   }
 }
